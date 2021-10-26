451c09b93f13f11494d06fb2dc7a16de0cd6dc11
Centauri07 <89342794+Centauri07@users.noreply.github.com>
2021-10-26 18:55:54 +0800

    Delete PrivateUtil.java

--- src/main/java/me/centauri07/mercury/util/PrivateUtil.java
+++
@@ -1,9 +0,0 @@
-package me.centauri07.mercury.util;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class PrivateUtil {
-    public final String TOKEN = "ODg0Njc2OTkwMTQ4NDIzNjkx.YTb9pg.GET6aa1TZbPlLaIcVY72JjDmgJk";
-    public final String DATABASE_PASSWORD = "a.U83UpVsQu#2LA";
-}
